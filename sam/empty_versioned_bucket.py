b4829ab7b0c6575342627edb897983106564a8e9
robkersey <rob@phazeone.com>
2024-03-15 12:42:16 +0000

    Update to tkae accoutn ID for delete and ignore .env

--- sam/empty_versioned_bucket.py
+++ sam/empty_versioned_bucket.py
@@ -1,7 +1,14 @@
 import boto3
+import sys 
+
+if len(sys.argv) < 2 : 
+    print('Please provide your account ID as an argument!')
+    sys.exit()
 
 #Edit the bucket name with your account ID
-BUCKET = 'uploads-tasks-app-us-east-1-{your account ID}'
+BUCKET = 'uploads-tasks-app-us-east-1-'+sys.argv[1]
+
+print('Emptying versioned bucket: ' + BUCKET)
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(BUCKET)
