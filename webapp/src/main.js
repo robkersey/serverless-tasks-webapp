86a42253cf2535a69eb114e261f3195cc951548f
robkersey <rob@phazeone.com>
2024-03-15 10:16:54 +0000

    Step3 Build & Deploy to Amplify

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import axios from 'axios'
 import router from './router'
 
-axios.defaults.baseURL = "PASTE API GATEWAY ENDPOINT HERE"
+axios.defaults.baseURL = " https://smh345qw2l.execute-api.us-east-1.amazonaws.com/v1"
 
 createApp(App).use(router).mount('#app')
